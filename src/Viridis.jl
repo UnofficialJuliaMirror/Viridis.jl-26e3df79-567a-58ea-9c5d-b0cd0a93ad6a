702947b6c221fa97a13e702a5ddbcbaaf3fcaded
Timothée Poisot <tim@poisotlab.io>
2016-11-06 17:53:01 -0500

    readme

--- src/Viridis.jl
+++ src/Viridis.jl
@@ -7,14 +7,16 @@ include(joinpath(".", "functions.jl"))
 include(joinpath(".", "colors.jl"))
 
 r_viridis = viridis_to_RGB.(_viridis_data)
-r_magma = viridis_to_RGB.(_magma_data)
-r_plasma = viridis_to_RGB.(_plasma_data)
-r_inferno = viridis_to_RGB.(_inferno_data)
-
 viridis = Scale.lab_gradient(r_viridis...)
-inferno = Scale.lab_gradient(r_inferno...)
-plasma = Scale.lab_gradient(r_plasma...)
+
+r_magma = viridis_to_RGB.(_magma_data)
 magma = Scale.lab_gradient(r_magma...)
+
+r_inferno = viridis_to_RGB.(_inferno_data)
+inferno = Scale.lab_gradient(r_inferno...)
+
+r_plasma = viridis_to_RGB.(_plasma_data)
+plasma = Scale.lab_gradient(r_plasma...)
 
 export viridis, magma, inferno, plasma
 
