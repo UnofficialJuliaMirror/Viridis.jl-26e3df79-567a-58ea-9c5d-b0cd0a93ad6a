fe24f0e35e59ed085829402416313f0b2e017959
Timothée Poisot <tim@poisotlab.io>
2016-11-06 17:47:48 -0500

    update with the raw palettes

--- src/Viridis.jl
+++ src/Viridis.jl
@@ -1,5 +1,23 @@
 module Viridis
 
-# package code goes here
+using Colors
+using Gadfly
+
+include(joinpath(".", "functions.jl"))
+include(joinpath(".", "colors.jl"))
+
+r_viridis = viridis_to_RGB.(_viridis_data)
+r_magma = viridis_to_RGB.(_magma_data)
+r_plasma = viridis_to_RGB.(_plasma_data)
+r_inferno = viridis_to_RGB.(_inferno_data)
+
+viridis = Scale.lab_gradient(r_viridis...)
+inferno = Scale.lab_gradient(r_inferno...)
+plasma = Scale.lab_gradient(r_plasma...)
+magma = Scale.lab_gradient(r_magma...)
+
+export viridis, magma, inferno, plasma
+
+
 
 end # module
