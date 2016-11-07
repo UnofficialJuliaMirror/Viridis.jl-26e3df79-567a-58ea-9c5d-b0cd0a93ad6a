1053ceb1cc8f3552759f56aa1ea007a9ddfb215e
Timothée Poisot <tim@poisotlab.io>
2016-11-07 10:26:23 -0500

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,12 @@ using Colors
 using Gadfly
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@test Viridis.plasma(0.0) == Lab(Viridis.viridis_to_RGB(Viridis._plasma_data[1]))
+@test Viridis.magma(0.0) == Lab(Viridis.viridis_to_RGB(Viridis._magma_data[1]))
+@test Viridis.viridis(0.0) == Lab(Viridis.viridis_to_RGB(Viridis._viridis_data[1]))
+@test Viridis.inferno(0.0) == Lab(Viridis.viridis_to_RGB(Viridis._inferno_data[1]))
 
-@test Viridis.plasma(0.0) == Viridis.viridis_to_RGB(Viridis._magma_data)[1]
+@test Viridis.plasma(1.0) == Lab(Viridis.viridis_to_RGB(Viridis._plasma_data[256]))
+@test Viridis.magma(1.0) == Lab(Viridis.viridis_to_RGB(Viridis._magma_data[256]))
+@test Viridis.viridis(1.0) == Lab(Viridis.viridis_to_RGB(Viridis._viridis_data[256]))
+@test Viridis.inferno(1.0) == Lab(Viridis.viridis_to_RGB(Viridis._inferno_data[256]))
